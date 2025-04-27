69ef179990d08fe871c3d4b9462b998b36873a79
amanparkashofficial <amanparkashofficial@gmail.com>
2025-04-27 17:00:01 +0500

    changes8

--- nlabstest/login.spec.ts
+++ nlabstest/login.spec.ts
@@ -1,30 +1,25 @@
-import {test, expect, Browser, Page, Locator} from '@playwright/test'
-import {webkit, chromium, firefox} from 'playwright'
+import { test, expect, Browser, Page, Locator } from '@playwright/test';
+import { webkit, chromium, firefox } from 'playwright';
 
-test('login test', async()=>{
-    //const browser:Browser = await firefox.launch({headless: false});
-    //const browser:Browser = await webkit.launch({headless: false});
-    const browser:Browser = await chromium.launch({headless: false, channel: 'chrome'});
-    const page:Page = await browser.newPage();
+test('login test', async () => {
+    //const browser: Browser = await firefox.launch({ headless: false });
+    //const browser: Browser = await webkit.launch({ headless: false });
+    const browser: Browser = await chromium.launch();
+
+    const page: Page = await browser.newPage();
     await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
-    
-    const emailId:Locator = await page.locator('#input-email');
-    const password:Locator = await page.locator('#input-password');
 
-    const LoginButton:Locator = await page.locator("[value=Login]");
+    const emailId: Locator = page.locator('#input-email');
+    const password: Locator = page.locator('#input-password');
+    const loginButton: Locator = page.locator("[value=Login]");
 
     await emailId.fill("apg@yopmail.com");
     await password.fill("playwright123");
-    await LoginButton.click();
+    await loginButton.click();
 
-    const title = await page.title();
-    console.log("home page title:", title);
+    await page.screenshot({ path: 'homepage.png' });
 
-    await page.screenshot({path:'homepage.png'});
+    await expect(page).toHaveTitle('My Account');
 
-    expect(title).toEqual('My Account');
-    //expect(title).toEqual('Account Login');
-
-    //browser.close();
-
+    await browser.close();
 });
