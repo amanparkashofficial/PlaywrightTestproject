dc16b97459d73ce27e04f50b1e25daf3be20915b
amanparkashofficial <amanparkashofficial@gmail.com>
2025-04-27 16:29:17 +0500

    changes1

--- nlabstest/register.spec.ts
+++ nlabstest/register.spec.ts
@@ -2,9 +2,9 @@ import {test, expect, Browser, Page, Locator} from '@playwright/test'
 import {webkit, chromium, firefox} from 'playwright'
 
 test('register test', async()=>{
-    //const browser:Browser = await firefox.launch({headless: false});
+    const browser:Browser = await firefox.launch({headless: false});
     //const browser:Browser = await webkit.launch({headless: false});
-    const browser:Browser = await chromium.launch({headless: false, channel: 'chrome'});
+    //const browser:Browser = await chromium.launch({headless: false, channel: 'chrome'});
     const page:Page = await browser.newPage();
     await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/register");
     
