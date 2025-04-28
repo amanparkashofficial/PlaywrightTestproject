04eaefb63d17ea939aa29b11eb47573a14730024
amanparkashofficial <amanparkashofficial@gmail.com>
2025-04-29 02:02:06 +0500

    add locator code

--- nlabstest/locator1.spec.ts
+++ nlabstest/locator1.spec.ts
@@ -4,7 +4,7 @@ import { webkit, chromium, firefox } from 'playwright';
 test('locator test', async () => {
     //const browser: Browser = await firefox.launch({ headless: false });
     //const browser: Browser = await webkit.launch({ headless: false });
-    const browser: Browser = await chromium.launch({ headless: false });
+    const browser: Browser = await chromium.launch();
 
     const page: Page = await browser.newPage();
     await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/register");
