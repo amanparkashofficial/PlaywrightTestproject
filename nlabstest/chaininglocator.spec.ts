53f68e7eebfe81d9d5399d6582679892ba6c9554
amanparkashofficial <amanparkashofficial@gmail.com>
2025-05-03 15:43:38 +0500

    add chaninglocator code

--- nlabstest/chaininglocator.spec.ts
+++ nlabstest/chaininglocator.spec.ts
@@ -3,9 +3,20 @@ import log from 'console';
 import {webkit, chromium, firefox } from 'playwright'
 
 test('locator test', async()=>{
-const browser: Browser = await chromium. launch();
+const browser: Browser = await chromium. launch({headless: false});
 const page: Page = await browser.newPage(); 
 await page.goto("https://www.orangehrm.com/30-day-free-trial/");
+
+await page.locator('form#Form_getForm >> #Form_getForm_Name').fill('APG');
+
+await page.locator('form#Form_getForm >>text=Get Your Free Trial').click();
+
+// const form = page. locator('form#Form_getForm');
+// const getYourFreeTrButton = page.getByRole('button', {name: 'Get Your Free Trial' });"
+await page. locator('form#Form_getForm').locator('#Form_getForm_Name').fill('APG');
+await page. locator('form#Form_getForm').getByRole('button', {name: 'Get Your Free Trial' }).click();
+//await form. locator (getYourFreeTrButton).click();
+
 await page.waitForTimeout(3000);
 
 
